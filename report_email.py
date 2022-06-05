3ab9687b79a2dad400c5c2c7e9f7101635e5489b
nitin <nitinpoorun@gmail.com>
2022-06-05 20:40:46 +0100

    report_email amended

--- report_email.py
+++ report_email.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
+import os
+import reports
 import emails
+from datetime import datetime
 
+def process_data(source_dir):
+  paragraph = ""
+  for file in os.listdir(source_dir):
+    with open(file, 'r') as f:
+      lines = f.readlines()
+      paragraph += f"name: {lines[0].strip()}<br/>weight: {lines[1].strip()}<br/><br/>"
+      f.close()
+  return paragraph
+  
 def main():
+  filename = "/tmp/processed.pdf"
+  title = "Processed Update on {}".format(format(datetime.now(), "%B %d, %Y"))
+  paragraph = process_data("./supplier-data/descriptions/")
+  reports.generate_report(filename, title, paragraph)
+
   sender = "automation@example.com"
   recipient = "username@example.com"
   subject = "Upload Completed - Online Fruit Store"
