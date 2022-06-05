eddd8b555f3039ce4c9ee1fcab29d0b796aa0f0e
nitin <nitinpoorun@gmail.com>
2022-06-05 20:19:14 +0100

    extra lines removed

---
+++ reports.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate_report(filename, title, paragraph):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  empty_line = Spacer(1,20)
+  paragraph = Paragraph(paragraph)
+  return report.build([report_title, empty_line, paragraph])
+
